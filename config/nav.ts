4e03ccf9bb62b73ba7351018879bb61833fbf2d4
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-13 14:04:48 -0400

    add navigation config

---
+++ config/nav.ts
@@ -0,0 +1,18 @@
+import { NavigationConfig } from "@/types";
+
+export const navigationConfig: NavigationConfig = {
+  mainNavigation: [
+    {
+      title: "Blind",
+      href: "/blind",
+    },
+    {
+      title: "Neet",
+      href: "/neet",
+    },
+    {
+      title: "SDE Sheet",
+      href: "/sheet",
+    },
+  ],
+};

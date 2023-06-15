18e3ac5d599771291196d8af86e793e04f60b6fc
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-15 13:45:57 -0400

    add navigation to (Main) layout

--- app/(Main)/layout.tsx
+++ app/(Main)/layout.tsx
@@ -1,3 +1,5 @@
+import { NavigationBar } from "@/components/navigation-bar";
+import { navigationConfig } from "@/config/nav";
 import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -5,17 +7,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="flex min-h-screen flex-col">
             <header className='container z-40 bg-background'>
                 <div className="flex h-20 items-center justify-start">
+                    <NavigationBar items={navigationConfig.mainNavigation} />
 
-                    {/* Main nav section - TODO: separate into own component */}
-                    <div className="flex gap-6 md:gap-10">
-                        <Link href="/" className="space-x-2 md:flex">
-                            <span>
-                                🔲
-                            </span>
-                            <span>Junior</span>
-                        </Link>
-
-                    </div>
                 </div>
             </header>
             <main className='flex-1'>{children}</main>

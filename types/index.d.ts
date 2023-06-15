2d42a85053889392effd1cdc8ea4c47612b6b79c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-15 14:19:48 -0400

    apply updated prettier/eslint config

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,19 +1,19 @@
 export type SiteConfig = {
-  name: string;
-  description: string;
-  url: string;
-  ogImage: string;
+  name: string
+  description: string
+  url: string
+  ogImage: string
   links: {
-    github: string;
-  };
-};
+    github: string
+  }
+}
 
 export type NavigationItem = {
-  title: string;
-  href: string;
-  disabled?: boolean;
-};
+  title: string
+  href: string
+  disabled?: boolean
+}
 
 export type NavigationConfig = {
-  mainNavigation: NavigationItem[];
-};
+  mainNavigation: NavigationItem[]
+}

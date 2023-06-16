749bd0c19171892a3c7fee33537ed4cba835f786
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-15 21:58:57 -0400

    style and add logo to nav

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,24 +1,43 @@
+"use client"
+
 import Link from "next/link"
+import { useSelectedLayoutSegment } from "next/navigation"
 import { NavigationItem } from "@/types"
+import { AlertDialogTrigger } from "@radix-ui/react-alert-dialog"
 
 import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
 
 interface NavigationProps {
   items: NavigationItem[]
 }
 
 export function NavigationBar({ items }: NavigationProps) {
+  const segment = useSelectedLayoutSegment()
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="items-center space-x-2 md:flex">
-        <span>☑️</span>
-        <span>{siteConfig.name}</span>
+      <Link href="/" className="hidden items-center space-x-2 md:flex">
+        <Icons.logo />
+        <span className="hidden font-bold sm:inline-block">
+          {siteConfig.name}
+        </span>
       </Link>
 
       {items?.length ? (
         <nav className="gap-6 md:flex">
           {items.map((item, index) => (
-            <Link key={index} href={item.disabled ? "#" : item.href}>
+            <Link
+              key={index}
+              href={item.disabled ? "#" : item.href}
+              className={cn(
+                "flex items-center text-lg font-medium transition-colors hover:text-foreground/80 sm:text-sm",
+                item.href.startsWith(`/${segment}`)
+                  ? "text-foreground"
+                  : "text-foreground/60",
+                item.disabled && "cursor-not-allowed opacity-80"
+              )}
+            >
               {item.title}
             </Link>
           ))}

a4cc853b1a7dc425596795722bd3b9c2c85e17aa
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-16 00:40:15 -0400

    add category types for questions

--- config/neet.ts
+++ config/neet.ts
@@ -12,3 +12,24 @@ export const neetConfig: NavigationConfig = {
     },
   ],
 }
+
+export const neetCategories = [
+  "Array & Hashing",
+  "Two Pointers",
+  "Stack",
+  "Sliding Window",
+  "Binary Search",
+  "Linked List",
+  "Trees",
+  "Tries",
+  "Heap & Priority Queue",
+  "Backtracking",
+  "Graphs",
+  "Advanced Graphs",
+  "Dynamic Programming (1 Dimension)",
+  "Dynamic Programming (2 Dimension)",
+  "Greedy",
+  "Intervals",
+  "Math & Geometry",
+  "Bit Manipulation",
+]
---
+++ config/sheet.ts
@@ -0,0 +1,17 @@
+export const sheetCategories = [
+  "Array & Hashing",
+  "Matrix",
+  "String",
+  "Searching & Sorting",
+  "LInkedlist",
+  "Binary Trees",
+  "Binary Search Trees",
+  "Greedy",
+  "Backtracking",
+  "Stacks & Queues",
+  "Heap",
+  "Graph",
+  "Trie",
+  "Dynamic Programming",
+  "Bit Manipulation",
+]

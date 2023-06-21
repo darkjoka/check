ec94f1eac0e0355bdc3a27a154b8f5981f09b7e5
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-21 02:24:11 -0400

    lengthen section page

--- app/(tasks)/(neet)/neet/page.tsx
+++ app/(tasks)/(neet)/neet/page.tsx
@@ -2,7 +2,8 @@ import { ProblemList } from "@/components/problem-list"
 
 export default function Neet() {
   return (
-    <section>
+    // make section longer than space available to prevent ui from shifting when accordion is opened
+    <section className="h-screen">
       <div className="container flex max-w-[64rem] flex-col">
         <ProblemList category="neet" />
       </div>

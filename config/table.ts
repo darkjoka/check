5f6136e82d17c3882470c3635275a87cff1e0a9e
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-21 01:56:46 -0400

    add configs for table

---
+++ config/table.ts
@@ -0,0 +1,14 @@
+export const head = [
+  {
+    heading: "Title",
+    accessor: "title",
+  },
+  {
+    heading: "Difficulty",
+    accessor: "difficulty",
+  },
+  {
+    heading: "My Rating",
+    accessor: "",
+  },
+]

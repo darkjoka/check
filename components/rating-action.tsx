359db1d61e7c7d3cb9fe1eccf0fbcf367d94866a
Junior Opoku-Ansah <60199291+darkjoka@users.noreply.github.com>
2023-06-21 16:30:41 -0400

    add rating action (#18)

---
+++ components/rating-action.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import {
+	Check,
+	MoreHorizontal,
+	PackageMinus,
+	PackagePlus,
+	PackageX,
+	RotateCcw,
+} from "lucide-react"
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuPortal,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+interface RatingActionProps {
+	rating: "" | "easy" | "medium" | "hard"
+}
+export function RatingAction({ rating }: RatingActionProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<MoreHorizontal />
+			</DropdownMenuTrigger>
+			<DropdownMenuPortal>
+				<DropdownMenuContent className="w-44">
+					<DropdownMenuItem className="space-x-3">
+						<div className="w-4" />
+						<span>Reset Rating</span>
+					</DropdownMenuItem>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem className="space-x-3">
+						<div className="w-4">
+							{rating == "easy" && <Check className="w-4" />}
+						</div>
+						<span>Easy</span>
+					</DropdownMenuItem>
+					<DropdownMenuItem className="space-x-3">
+						<div className="w-4">
+							{rating == "medium" && <Check className="w-4" />}
+						</div>
+						<span>Medium</span>
+					</DropdownMenuItem>
+					<DropdownMenuItem className="space-x-3">
+						<div className="w-4">
+							{rating == "hard" && <Check className="w-4" />}
+						</div>
+						<span>Hard</span>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenuPortal>
+		</DropdownMenu>
+	)
+}

c925ae0192ff217f1baf9999be794185de8903f6
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-22 01:44:19 -0400

    add useRatingStore

---
+++ store/rating.ts
@@ -0,0 +1,85 @@
+import { Data, Difficulty } from "@/types"
+import { create } from "zustand"
+import { createJSONStorage, persist, StateStorage } from "zustand/middleware"
+
+type RatingData = {
+	difficulty: Difficulty
+	time: Date
+}
+
+type RatingState = {
+	ratings: Map<string, RatingData>
+}
+
+type RatingAction = {
+	rate: (signature: string, difficulty: Difficulty) => void
+	reset: (signature: string) => void
+	resetAll: VoidFunction
+}
+
+const storage = {
+	getItem: (name: string) => {
+		console.log(name, "has been retrieved")
+		const str = localStorage.getItem(name) ?? ""
+		return {
+			state: {
+				...JSON.parse(str).state,
+				ratings: new Map(Object.entries(JSON.parse(str).state.ratings)),
+			},
+		}
+	},
+
+	setItem: (name: string, value: { state: RatingState }) => {
+		console.log(name, "has been set with", value)
+		const str = JSON.stringify({
+			state: {
+				...value.state,
+				ratings: Object.fromEntries(value.state.ratings),
+			},
+		})
+		localStorage.setItem(name, str)
+	},
+
+	removeItem: (name: string) => {
+		console.log(name, "has been deleted")
+		localStorage.removeItem(name)
+	},
+}
+
+export const useRatingStore = create<RatingState & RatingAction>()(
+	persist(
+		(set, get) => ({
+			ratings: new Map(),
+
+			rate: (signature, difficulty) =>
+				set((prev) => {
+					const rating = {
+						...(prev.ratings.get(signature) ?? {
+							difficulty,
+							time: undefined,
+						}),
+						difficulty,
+						time: new Date(),
+					}
+					return {
+						ratings: new Map(prev.ratings).set(signature, rating),
+					}
+				}),
+
+			reset: (signature) =>
+				set((prev) => {
+					const ratings = new Map(prev.ratings)
+					ratings.delete(signature)
+					return {
+						ratings,
+					}
+				}),
+
+			resetAll: () => set({ ratings: new Map() }),
+		}),
+		{
+			name: "rating-store",
+			storage,
+		}
+	)
+)

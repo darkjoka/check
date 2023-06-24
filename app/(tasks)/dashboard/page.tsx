3c0f6f4fa10c20b62b106c20c462d23e65473a6c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-24 05:52:02 -0400

    add previous and summary to dashboard

--- app/(tasks)/dashboard/page.tsx
+++ app/(tasks)/dashboard/page.tsx
@@ -1,7 +1,23 @@
+import { Previous } from "@/components/previous"
+import { SummaryCard } from "@/components/summary-card"
+
 export default function Dashboard() {
   return (
     <section>
-      <div className="container flex flex-col">Welcome to your Dashboard</div>
+      <div className="container flex flex-col"></div>
+      <div className="container my-2 flex items-center justify-center">
+        <h1 className="font-heading text-5xl">Dashboard</h1>
+      </div>
+      <div className="container flex">
+        <div className="flex-[2_2_0%]">
+          <div className="grid grid-cols-2">
+            <SummaryCard />
+          </div>
+        </div>
+        <div className="flex-1">
+          <Previous />
+        </div>
+      </div>
     </section>
   )
 }

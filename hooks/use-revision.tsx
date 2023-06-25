e0c8ff429fab0a0acb28877e31720178057be77b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-25 18:36:21 -0400

    add use revision hook

---
+++ hooks/use-revision.tsx
@@ -0,0 +1,19 @@
+import { PreviousStore, usePreviousStore } from "@/store"
+import PreviousMap from "postcss/lib/previous-map"
+
+const daysBetweenAttempt = 3
+
+const dataSelector = (state: PreviousStore) => state.data
+export function useRevision(count = 5) {
+	const allPrevious = usePreviousStore(dataSelector)
+	// remove all questions less than 3 days old
+	const validPrevious = allPrevious.filter(
+		({ time, difficulty }) =>
+			Math.floor(
+				(new Date().getTime() - new Date(time).getTime()) /
+					(24 * 60 * 60 * 1000)
+			) >= daysBetweenAttempt && difficulty !== "easy"
+	)
+
+	return validPrevious.slice(0, count)
+}

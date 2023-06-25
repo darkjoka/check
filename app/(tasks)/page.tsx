3085fb80511dcac44f2e1bac666a9b4119dabeba
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-25 18:53:58 -0400

    add components to home

--- app/(tasks)/page.tsx
+++ app/(tasks)/page.tsx
@@ -1,7 +1,17 @@
+import { Recommendation } from "@/components/recommendation"
+import { Revision } from "@/components/revision"
+
 export default function Home() {
   return (
     <section>
-      <div className="container flex flex-col">Welcome to your daily Tasks</div>
+      <div className="container mt-4 flex max-w-[65rem] flex-col">
+        <h1 className="flex justify-center font-heading text-4xl">Revisions</h1>
+        <Revision />
+        <h1 className="mt-4 flex justify-center font-heading text-4xl">
+          Recommendations
+        </h1>
+        <Recommendation />
+      </div>
     </section>
   )
 }

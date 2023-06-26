0a976714752d0f9d15967ec2944886d541475f28
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-25 20:46:51 -0400

    update styles for previous

--- components/previous.tsx
+++ components/previous.tsx
@@ -12,7 +12,7 @@ interface PreviousProps {
 }
 
 const problemsSelector = (store: PreviousStore) => store.data
-export function Previous({ count = 10 }: PreviousProps) {
+export function Previous({ count = 8 }: PreviousProps) {
 	const mounted = useMounted()
 	const problems = usePreviousStore(problemsSelector)
 
@@ -20,19 +20,21 @@ export function Previous({ count = 10 }: PreviousProps) {
 		mounted && (
 			<div
 				className={cn(
-					"flex w-full flex-col rounded border border-border p-4",
-					!problems.length && "h-96 items-center justify-center"
+					"flex  w-full flex-col rounded border border-border p-4",
+					!problems.length && "items-center justify-center"
 				)}
 			>
 				{problems.length ? (
-					<div className="h-[37rem] space-y-3">
-						<p className="font-heading text-2xl">Recent Problems</p>
-						{problems
-							.slice(-count)
-							.reverse()
-							.map((data, key) => (
-								<PreviousItem key={key} {...{ data }} />
-							))}
+					<div className="h-[28.85rem]">
+						<p className="p-0 font-heading text-2xl">Recent Problems</p>
+						<div className="space-y-3">
+							{problems
+								.slice(-count)
+								.reverse()
+								.map((data, key) => (
+									<PreviousItem key={key} {...{ data }} />
+								))}
+						</div>
 					</div>
 				) : (
 					<span className="text-sm text-muted-foreground">

f864df71bddb4ab7cc42a76a8212898ed70002c3
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-25 20:48:12 -0400

    add dash components to dashboard page

--- app/(tasks)/dashboard/page.tsx
+++ app/(tasks)/dashboard/page.tsx
@@ -1,3 +1,6 @@
+import { Categories } from "@/config/categories"
+import { Dash, MiniDash } from "@/components/dash"
+import { Dashcard } from "@/components/dash-card"
 import { Previous } from "@/components/previous"
 import { SummaryCard } from "@/components/summary-card"
 
@@ -8,16 +11,18 @@ export default function Dashboard() {
       <div className="container my-2 flex items-center justify-center">
         <h1 className="font-heading text-5xl">Dashboard</h1>
       </div>
-      <div className="container flex">
+      <div className="container mb-2 flex space-x-2">
         <div className="flex-[2_2_0%]">
-          <div className="grid grid-cols-2">
+          <div className="grid grid-cols-2 gap-2">
             <SummaryCard />
+            <MiniDash />
           </div>
         </div>
         <div className="flex-1">
           <Previous />
         </div>
       </div>
+      <Dash />
     </section>
   )
 }

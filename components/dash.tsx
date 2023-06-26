43e29476e68182463ebfb0800237ce64483ea093
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-25 20:47:49 -0400

    update and coplete dash component

--- components/dash.tsx
+++ components/dash.tsx
@@ -1,20 +1,40 @@
 "use client"
 
+import data from "@/data/data.json"
+
+import { Categories } from "@/config/categories"
 import { useStats } from "@/hooks/use-stats"
 import { Dashcard } from "@/components/dash-card"
 
 export function Dash() {
-	const stats = useStats()
+	const { categories, allCategories } = useStats()
+
 	return (
-		<div>
-			<div className="flex w-full flex-1 bg-blue-200">
-				<div className="h-48 flex-1 border border-border">a</div>
-				<div className="h-48 flex-1 border border-border">b</div>
-			</div>
-			<div className="flex flex-1">
-				<div className="flex-1"></div>
-				<div className="flex-1"></div>
-			</div>
+		<div className="container grid grid-cols-3 gap-2">
+			{Categories.slice(2).map((value, key) => (
+				<Dashcard
+					category={value}
+					num={categories.get(value)!}
+					denum={allCategories.get(value)!}
+					key={key}
+				/>
+			))}
+		</div>
+	)
+}
+
+export function MiniDash() {
+	const { categories, allCategories } = useStats()
+	return (
+		<div className="space-y-2">
+			{Categories.slice(0, 2).map((value, key) => (
+				<Dashcard
+					category={value}
+					num={categories.get(value)!}
+					denum={allCategories.get(value)!}
+					key={key}
+				/>
+			))}
 		</div>
 	)
 }

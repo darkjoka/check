a50e3265251b7331120985ca6fb407608b7b36db
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-25 20:47:23 -0400

    finish dash card component

--- components/dash-card.tsx
+++ components/dash-card.tsx
@@ -1,42 +1,52 @@
+"use client"
+
+import { useMounted } from "@/hooks/use-mounted"
+import { useStats } from "@/hooks/use-stats"
+import { Ring } from "@/components/ring"
+
 interface DashcardProps {
-	overallCompleted: number
-	overall: number
-	title: string
+	category: string
+	num: number
+	denum: number
 }
 
-export function Dashcard({ overallCompleted, overall, title }: DashcardProps) {
+export function Dashcard({ category, num, denum }: DashcardProps) {
+	const mounted = useMounted()
 	return (
-		<div className="h-48 w-full flex-1 rounded border border-border">
-			<p className="text-xl font-semibold text-muted-foreground">{title}</p>
-			<div className="flex justify-center">
-				<div>
-					<p className="font-heading text-5xl">
-						{Math.round((overallCompleted / overall) * 100)}{" "}
-						<span className="text-2xl text-muted-foreground">%</span>
-					</p>
-					<p className="flex items-center text-sm font-semibold text-muted-foreground">
-						Goal Completion
-					</p>
-				</div>
-			</div>
-			<div className="container flex justify-between">
-				<div>
-					<p className="font-heading text-4xl">
-						24 <span className="text-xl text-muted-foreground">%</span>
-					</p>
-					<p className="text-xs font-semibold text-muted-foreground">
-						Problems Solved
-					</p>
-				</div>
-				<div>
-					<div>
-						<p className="font-heading text-4xl">
-							24 <span className="text-xl text-muted-foreground">/115</span>
-						</p>
-						<p className="text-xs font-semibold text-muted-foreground"></p>
+		mounted && (
+			<div className=" w-full flex-1 rounded border border-border p-4">
+				<p className="my-2 text-xl font-semibold text-muted-foreground">
+					{category}
+				</p>
+				<div className="flex items-center justify-between">
+					<div className="space-y-4">
+						<div>
+							<p className="font-heading text-5xl">
+								{Math.round((num / denum) * 100)}
+								<span className="text-2xl text-muted-foreground"> %</span>
+							</p>
+							<p className="flex items-center text-sm font-semibold text-muted-foreground">
+								Goal Completion
+							</p>
+						</div>
+						<div>
+							<p className="font-heading text-5xl">
+								{num}
+								<span className="text-2xl text-muted-foreground">
+									{" "}
+									/ {denum}
+								</span>
+							</p>
+							<p className="flex items-center text-sm font-semibold text-muted-foreground">
+								Problems Completetd
+							</p>
+						</div>
+					</div>
+					<div className="flex justify-center py-2">
+						<Ring angle={(num / denum) * 359} />
 					</div>
 				</div>
 			</div>
-		</div>
+		)
 	)
 }

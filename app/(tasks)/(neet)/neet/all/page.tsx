532fd63c6cb64e2229e7162c5babc475374c0c2f
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2023-06-26 23:11:47 -0400

    add headings to pages

--- app/(tasks)/(neet)/neet/all/page.tsx
+++ app/(tasks)/(neet)/neet/all/page.tsx
@@ -3,6 +3,9 @@ import { ProblemList } from "@/components/problem-list"
 export default function NeetAll() {
   return (
     <section className="h-screen">
+      <h1 className="my-2 flex flex justify-center font-heading text-5xl">
+        Neet All
+      </h1>
       <div className="container flex max-w-[64rem] flex-col">
         <ProblemList category="neet all" />
       </div>
